18917af0a46844d8c99d94133a8c2334a2e0b87f
PG Bhai <prabhugantayat@Prabhus-Air.mshome.net>
2020-09-13 20:00:33 +0530

    Added web-build

---
+++ web-build/precache-manifest.1b39b717e8a7d0f77093d9896a7c8a1b.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = [
+  {
+    "revision": "d6a8a5bb25c19e1d4da8",
+    "url": "/static/js/app.bf091aa9.chunk.js"
+  },
+  {
+    "revision": "66090dc4495d4ce3dfb9",
+    "url": "/static/js/runtime~app.2e9f1821.js"
+  },
+  {
+    "revision": "bd460085dd4390d1fdab",
+    "url": "/static/js/2.5a9a6c4f.chunk.js"
+  },
+  {
+    "revision": "e29f358b53eb2bc66d23151b85b106ad",
+    "url": "/static/media/pg.7bc00dc0.png"
+  },
+  {
+    "revision": "f43bc33d97c813f08d6609bd88b1ce92",
+    "url": "/static/media/lom.c85708f0.png"
+  },
+  {
+    "revision": "4761cb6a288ad4f08eb794416be1c4bf",
+    "url": "/static/media/sat.59b1c59b.png"
+  },
+  {
+    "revision": "f971eb407b1ff8c759d7e92be871a119",
+    "url": "/static/media/sar.ebb3cc34.png"
+  },
+  {
+    "revision": "148e6275552c7cd72f327465d48bf9d5",
+    "url": "/static/media/pat.d6acdcf0.png"
+  },
+  {
+    "revision": "a037ac9f7a4c665f9c93fede5c239a50",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ec543248d7b23864564429fc03837190",
+    "url": "/serve.json"
+  },
+  {
+    "revision": "2002987acac9deb6803ab4ba381226ba",
+    "url": "/expo-service-worker.js"
+  },
+  {
+    "revision": "e802d2644326fd9fa701f1d969a8a94a",
+    "url": "/register-service-worker.js"
+  },
+  {
+    "revision": "a990a8c98c379b1c14de33443329d80a",
+    "url": "/static/js/2.5a9a6c4f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0f9d9d648ecd88947d3afbec435af97f",
+    "url": "/favicon-16.png"
+  },
+  {
+    "revision": "9135f62b0367cc21a50e4578a2c71f51",
+    "url": "/favicon-32.png"
+  },
+  {
+    "revision": "6e859e27a814dfd8ac8fb2ecfd1e73c6",
+    "url": "/favicon.ico"
+  },
+  {
+    "revision": "680f90c6e40936f480891501848832be",
+    "url": "/manifest.json"
+  }
+];
